c592f51dbaf3f3b24ecd9f56203aad1b4c6c4298
ryanmcdonough <me@ryanmcdonough.co.uk>
2014-08-13 11:45:42 +0100

    Create CrowdSSOAttribute.cs

---
+++ CrowdSSOAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class CrowdSSOAttribute
+    {
+        public string Name { get; set; }
+        public object Values { get; set; }
+    }
