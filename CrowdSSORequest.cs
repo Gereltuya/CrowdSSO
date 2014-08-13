159494fe518c6d32bfdffbad3962a3fbffa80a0e
ryanmcdonough <me@ryanmcdonough.co.uk>
2014-08-13 11:45:59 +0100

    Create CrowdSSORequest.cs

---
+++ CrowdSSORequest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class CrowdSSORequest
+    {
+        public CrowdSSOAPICall apiCall { get; set; }
+        public string method { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string group { get; set; }
+       
+    }
